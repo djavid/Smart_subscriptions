c0572cc21923933cfcd768bf799feac28ba9ded2
Dzhavid Khalilov <d.khalilov@nicity.com>
2021-10-28 20:14:27 +0300

    added shared module

---
+++ iosApp/iosApp/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosAppApp.swift
+//  iosApp
+//
+//  Created by Джавид Халилов on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
